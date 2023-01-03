71402a4e217ecf7c8a3d446c0f12d1c66ce2b44c
King Paulus <mergetp02@gmail.com>
2023-01-03 19:03:02 +0100

    First Comiit

---
+++ src/Java_Adventure_Game/Adventure_Main.java
@@ -0,0 +1,12 @@
+package Java_Adventure_Game;
+
+public class Adventure_Main {
+	static int BreiteMain = 500;
+	static int HoheMain = 500;
+	static String GameName = "Tales of Zentunia";
+
+	public static void main(String[] args) {
+		GameMode gm = new GameMode();
+	}
+
+}
